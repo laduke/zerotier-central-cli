cba4bac3fef2e5c8b2741d14a2a8e7a74867eb2b
Travis LaDuke <travisladuke@gmail.com>
2020-02-28 14:30:32 -0800

    convert tests to teenytest
    going to use testdouble mocks; teenytest before/after config is useful

--- src/network/network-object.test.js
+++ src/network/network-object.test.js
@@ -1,71 +1,63 @@
-const test = require('tape')
+const assert = require('assert')
 
 const { Network } = require('./network-object.js')
 
-test('empty', t => {
-  const n = Network.fromObj()
-  t.deepEqual(n.toJSON(), {})
+module.exports = {
+  empty: () => {
+    const n = Network.fromObj()
+    assert.deepStrictEqual(n.toJSON(), {})
+  },
 
-  t.end()
-})
+  'one thing': () => {
+    const description = '2'
+    const network = Network.fromObj({ description })
 
-test('one thing', t => {
-  const description = '2'
-  const network = Network.fromObj({ description })
+    assert.ok(!network.toJSON().config)
+    assert.equal(description, network.description)
+  },
 
-  t.notOk(network.toJSON().config)
-  t.equal(description, network.description)
+  merge: () => {
+    const network = Network.fromObj({
+      name: 'g',
+      description: 'd',
+      private: false,
+      multicastLimit: 0
+    })
+    const network2 = network.merge({
+      name: 'k',
+      description: null,
+      private: true,
+      multicastLimit: 1
+    })
 
-  t.end()
-})
+    assert.equal(network2.name, 'k')
+    assert.equal(network2.description, 'd')
+    assert.equal(network2.private, true)
+    assert.equal(network2.multicastLimit, 1)
+  },
 
-test('merge', t => {
-  const network = Network.fromObj({
-    name: 'g',
-    description: 'd',
-    private: false,
-    multicastLimit: 0
-  })
-  const network2 = network.merge({
-    name: 'k',
-    description: null,
-    private: true,
-    multicastLimit: 1
-  })
+  'it ignores weird keys': () => {
+    const network = Network.fromObj()
 
-  t.equal(network2.name, 'k')
-  t.equal(network2.description, 'd')
-  t.equal(network2.private, true)
-  t.equal(network2.multicastLimit, 1)
+    const network2 = network.merge({ foojiwooji: 1 })
 
-  t.end()
-})
+    assert.deepStrictEqual(network2.toJSON(), {})
+    assert.ok(network2 instanceof Network)
+  },
 
-test('it ignores weird keys', t => {
-  const network = Network.fromObj()
+  'round trip': () => {
+    const json = require('./sample.js')
 
-  const network2 = network.merge({ foojiwooji: 1 })
+    const nw1 = Network.fromJSON(json)
+    const nw2 = Network.fromObj(nw1)
+    const nw3 = Network.fromJSON(nw2.toJSON())
 
-  t.deepEqual(network2.toJSON(), {})
-  t.ok(network2 instanceof Network)
+    for (const key of Network.keys()) {
+      assert.deepStrictEqual(nw1[key], nw2[key], `a ${key}`)
+    }
 
-  t.end()
-})
-
-test('round trip', t => {
-  const json = require('./sample.js')
-
-  const nw1 = Network.fromJSON(json)
-  const nw2 = Network.fromObj(nw1)
-  const nw3 = Network.fromJSON(nw2.toJSON())
-
-  for (const key of Network.keys()) {
-    t.deepEqual(nw1[key], nw2[key], `a ${key}`)
+    for (const key of Network.keys()) {
+      assert.deepStrictEqual(nw1[key], nw3[key], `b ${key}`)
+    }
   }
-
-  for (const key of Network.keys()) {
-    t.deepEqual(nw1[key], nw3[key], `b ${key}`)
-  }
-
-  t.end()
-})
+}
