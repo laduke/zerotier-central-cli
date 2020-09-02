c871a1e09c207cbddfdacb46df0ce9f08199fdde
Travis LaDuke <travisladuke@gmail.com>
2020-09-02 10:25:06 -0700

    Add Network Delete command

---
+++ src/commands/network/delete.js
@@ -0,0 +1,39 @@
+const axios = require('axios').default
+const { flags } = require('@oclif/command')
+
+const Command = require('../../api-base.js')
+
+class SetNetwork extends Command {
+  async run () {
+    const { flags } = this.parse(SetNetwork)
+    const {
+      args: { networkId }
+    } = this.parse(SetNetwork)
+
+    const req = this.central.networkDelete(networkId)
+    if (flags['dry-run']) {
+      this.log(`dry run: delete ${networkId}`)
+      if (flags.json) {
+        this.log(req)
+      }
+      return
+    }
+
+    const data = await axios(req)
+
+    this.log(`deleted ${networkId}`)
+    if (flags.json) {
+      this.log(req)
+    }
+  }
+}
+
+SetNetwork.description = 'delete a network'
+SetNetwork.args = [{ name: 'networkId', required: true }]
+
+SetNetwork.flags = {
+  ...Command.flags,
+  'dry-run': flags.boolean({ char: 'n' })
+}
+
+module.exports = SetNetwork
